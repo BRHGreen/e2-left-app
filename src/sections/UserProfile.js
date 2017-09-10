e153c09805319f04376454d617fb82dee5ad98a2
BRHGreen <benedictgreen@gmail.com>
2017-09-10 14:54:39 +0100

    firstName rendering on user profile

--- src/sections/UserProfile.js
+++ src/sections/UserProfile.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 class UserProfile extends Component {
   render () {
+    // const { firstName, lastName } = this.props.data.user
     console.log('user props: ', this.props);
+    if (!this.props.data.loading) {
+      console.log('user props data: ', this.props.data.user.firstName);
+    }
     return (
-      <p>user profile</p>
+      <div>
+        {!this.props.data.loading &&
+          <p>Hello {this.props.data.user.firstName}</p>
+        }
+      </div>
     )
   }
 }
