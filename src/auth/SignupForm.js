5912855040eb75a75306ee97d7c3177d0f025ba8
BRHGreen <benedictgreen@gmail.com>
2017-09-19 01:11:37 +0100

    working if directed to profile

--- src/auth/SignupForm.js
+++ src/auth/SignupForm.js
@@ -14,7 +14,7 @@ class SignupForm extends Component {
 
   componentWillUpdate (nextProps) {
     if (!this.props.data.user && nextProps.data.user) {
-        hashHistory.push('/dashboard')
+        hashHistory.push('/user-profile')
     }
   }
 
